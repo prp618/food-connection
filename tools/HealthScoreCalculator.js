aa7991b195cd1c58258a8200ffc32854e1e3d4a9
Doombleflap <54334402+Doombleflap@users.noreply.github.com>
2020-10-19 20:12:31 -0400

    Update HealthScoreCalculator.js
    Added documentation to HealthScore Calulator.js

--- tools/HealthScoreCalculator.js
+++ tools/HealthScoreCalculator.js
@@ -1,3 +1,5 @@
+// Documentation created by Andrew Glenn
+// Preset values needed to run the Health Score Calculator
 const PERCENT_CARBS         = .55;
 const PERCENT_PROTEIN       = .15;
 const PERCENT_FAT           = .30;
@@ -11,20 +13,22 @@ const FAT_MAX_POINTS        = PERCENT_FAT * 100;
 const PROTEIN_MAX_POINTS    = PERCENT_PROTEIN * 100;
 const OVEREATING_PENALTY    = 3;
 
+// Main class
 export default class HealthScoreCalculator {
+    //Constructor used in LoginPage.js (components) and SignUpLoadPage.js (components)
     static createDiet(user) {
-        const currentDay = new Date();
-        currentDay.setHours(0, 0, 0, 0);
-        const diet = {
+        const currentDay = new Date(); // Pulls date from PC's time
+        currentDay.setHours(0, 0, 0, 0); //https://www.w3schools.com/jsref/jsref_sethours.asp
+        const diet = { // constant reference to the value diet, used in LoginPage.js and SignUpLoadPage.js
             total: 0,
             fat: 0,
             protein: 0,
             carbohydrates: 0,
             lastCalculated: currentDay,
         };
-        const age = user.age;
+        const age = user.age; // constant reference to the value age, created in users.js
         const physicalActivityCoefficient = 1.0;
-        switch (user.activityLevel) {
+        switch (user.activityLevel) { // Switch case used to determine "physical activity", referenced in HealthScoreCalculator only
             case "sedentary":
                 physicalActivityCoefficient = 1.0;
                 break;
@@ -38,6 +42,7 @@ export default class HealthScoreCalculator {
                 physicalActivityCoefficient = 1.45;
                 break;
         }
+        // Calculations to determine the "diet" value
         const weight = user.weight * 0.4535924; /* Pounds to kilos */
         const height = user.height * 0.0254; /* Inches to meters */
         diet.total = Math.round(354.1 - (6.91 * age) + physicalActivityCoefficient * (9.36 * weight + 726 * height));
@@ -47,16 +52,19 @@ export default class HealthScoreCalculator {
         return diet;
     }
 
+    // Constructor used in FoodPage.js (components), LoginPage.js (components),
+    //                     and SignUpLoadPage.js
     static setHealthScore(foods, diet) {
-        foods.forEach((food) => {
+        foods.forEach((food) => { // food referenced from foods.js
             food.healthScore = 0;
 
-            if (food.addedSugar) {
+            if (food.addedSugar) { // If sugar is added in food, apply sugar penalty
                 food.healthScore -= ADDED_SUGAR_PENALTY;
             }
 
-            food.healthScore += food.fiber * FIBER_BONUS_PER_GRAM;
+            food.healthScore += food.fiber * FIBER_BONUS_PER_GRAM; // Adds the "fiber" score to the Health Score
 
+            // Calculations ran to determine values used later in the HealthScoreCalculator.js
             const projectedCalories = diet.total / food.calories;
             const carbRatio = (food.carbs * CALORIES_PER_GRAM_C * projectedCalories) / diet.carbohydrates;
             const proteinRatio = (food.protein * CALORIES_PER_GRAM_P * projectedCalories) / diet.protein;
@@ -67,6 +75,8 @@ export default class HealthScoreCalculator {
             const fatDistance = 1 - fatRatio;
 
             const caloriesOver = food.calories - diet.total;
+
+            // A collection of IF statements to continue calculating the Health Score
             if (caloriesOver > 0) {
                 food.healthScore -= (caloriesOver / 10) * OVEREATING_PENALTY;
             }
@@ -90,9 +100,11 @@ export default class HealthScoreCalculator {
             food.healthScore = Math.round(food.healthScore);
         });
 
+        // When called, compares two food Health Score values and returns the results
         foods.sort((a, b) => {
             return b.healthScore - a.healthScore;
         });
+        // Returns the foods value to wherever it was called
         return foods;
     }
 }
