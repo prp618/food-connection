eb3250a214779b894f3eb9a589c47bfff5afffff
Doombleflap <54334402+Doombleflap@users.noreply.github.com>
2020-10-22 22:50:09 -0400

    Update foods.js
    NEEDS ANOTHER PAIR OF EYES TO LOOK THROUGH
    backend/routes/foods.js updated

--- backend/routes/foods.js
+++ backend/routes/foods.js
@@ -1,12 +1,19 @@
+// Documentation by Andrew Glenn 10/22/2020
+
+// Constant 'router' created to route schema models to the database
 const router = require('express').Router();
+// The 'Food' model from 'food.models.js' is called
 const Food = require('../models/food.model');
 
+// Retrieves 'Food' data from the database to determine errors
+// (Needs more information on this, I believe that this catches errors for duplicates)
 router.route('/').get((req, res) => {
     Food.find()
         .then(foods => res.json({ result: 1, foods: foods }))
         .catch(err => res.status(400).json({ result: 0, message: 'Error' + err }));
 });
 
+// Posts foods to the database, if any exceptions are caught, an error will be posted.
 router.route('/add').post((req, res) => {
     const food = req.body.food;
     const newFood = new Food(food);
