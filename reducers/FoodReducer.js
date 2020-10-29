363f1e7c71486339fe4e6d07e1acfdd6bf038619
prp618 <59840199+prp618@users.noreply.github.com>
2020-10-29 14:13:32 -0400

    Update FoodReducer.js

--- reducers/FoodReducer.js
+++ reducers/FoodReducer.js
@@ -1,19 +1,26 @@
+//Documentation by Brandon Bunce
+//sets INITIAL_FOOD_STATE as a list of data
 const INITIAL_FOOD_STATE = { 
     list: [],
 }
 
+//foodReducer is initialized by populating the initial food state with data
 const foodReducer = (state = INITIAL_FOOD_STATE, action) => {
     switch (action.type) {
+        //if action type is SET_FOODS, then populate INITIAL_FOOD_STATE by performing action.payload
         case 'SET_FOODS':
             state = {
                 ...state,
                 list: action.payload,
             }
             break;
+        //if action type is not SET_FOODS, do nothing
         default:
             break;
     }
+    //returns either default or applies action to the selected food
     return state;
 }
 
+//exports this module to be accessible by the rest of the program
 export default foodReducer;
