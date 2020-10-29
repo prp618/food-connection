d786c92c3b4e67ff28bb26288564bf4bb562f8d9
prp618 <59840199+prp618@users.noreply.github.com>
2020-10-29 14:06:34 -0400

    Update GlobalReducer.js

--- reducers/GlobalReducer.js
+++ reducers/GlobalReducer.js
@@ -1,19 +1,25 @@
+//documentation by Brandon Bunce
+//Set INITIAL_GLOBAL_STATE to currently slected food
 const INITIAL_GLOBAL_STATE = {
     selectedFood: {},
 }
 
+//perform reducer action based on selected food
 const globalReducer = (state = INITIAL_GLOBAL_STATE, action) => {
     switch (action.type) {
+        //if action = SELECT_FOOD, 
         case 'SELECT_FOOD':
             state = {
                 ...state,
                 selectedFood: action.payload,
             }
             break;
+        //in default case, do nothing
         default:
             break;
     }
     return state;
 }
 
+//exports this object, making it accessbile for other modules and scripts in this program
 export default globalReducer;
